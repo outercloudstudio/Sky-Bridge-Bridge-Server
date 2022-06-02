74f3ff691672802dabaf1ecea9f7fd9a19ba4b1e
outercloudstudio <outercloudstudio@gmail.com>
2022-06-02 02:50:26 -0500

    First Connection

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace BridgeServer
+{
+    class Program
+    {
+        private static int port = 25565;
+
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for connection...");
+                TcpClient client = listener.AcceptTcpClient();
+
+                Console.WriteLine("Connection accepted!");
+                NetworkStream networkStream = client.GetStream();
+
+                //SkyBridge.me.connections.Add(new Connection(client, networkStream, client.Client.RemoteEndPoint.ToString(), false));
+            }
+        }
+    }
+}
