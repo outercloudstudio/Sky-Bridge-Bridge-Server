8b6c774b807e2c019a7f5897dc94ce767835cbed
Outer Cloud Studio <outercloudstudio@gmail.com>
2022-06-11 13:27:21 -0500

    Updated Room Management

--- Program.cs
+++ Program.cs
@@ -14,6 +14,23 @@ namespace BridgeServer
         public class Room
         {
             public string ID;
+            public Connection[] connections;
+
+            public Room(string _ID, int maxConnections)
+            {
+                ID = _ID;
+                connections = new Connection[maxConnections];
+            }
+
+            public int GetOpenIndex()
+            {
+                for (int i = 0; i < connections.Length; i++)
+                {
+                    if (connections[i] == null) return i;
+                }
+
+                return -1;
+            }
         }
 
         private static int port = 25565;
@@ -35,24 +52,69 @@ namespace BridgeServer
             listenThread = new Thread(ListenForConnections);
             listenThread.Start();
 
+            float tickDelay = 1f / 60f;
+
             while (true)
             {
+                for (int i = 0; i < rooms.Count; i++)
+                {
+                    Room room = rooms[i];
+
+                    if (room == null) continue;
+
+                    for (int j = 1; j < room.connections.Length; j++)
+                    {
+                        Connection connection = room.connections[i];
+
+                        if (connection == null) continue;
+
+                        connection.Update(tickDelay);
+
+                        if (connection.connectionMode == Connection.ConnectionMode.DISCONNECTED)
+                        {
+                            room.connections[i] = null;
+
+                            Console.WriteLine("Removed connection " + connection.IP + ":" + connection.port + " from room " + room.ID );
+                        }
+                    }
+
+                    if (room.connections[0].connectionMode == Connection.ConnectionMode.DISCONNECTED)
+                    {
+                        for (int j = i; j < room.connections.Length; j++)
+                        {
+                            Connection connection = room.connections[i];
+
+                            if (connection == null || connection.connectionMode != Connection.ConnectionMode.CONNECTED) continue;
+
+                            connection.Disconnect();
+
+                            Console.WriteLine("Disconnected connection " + connection.IP + ":" + connection.port + " from room " + room.ID + " because host disconnected");
+                        }
+
+                        rooms.RemoveAt(i);
+                        i--;
+
+                        Console.WriteLine("Removed room " + room.ID);
+                    }
+                }
+
                 for (int i = 0; i < lobbyConnections.Length; i++)
                 {
                     Connection connection = lobbyConnections[i];
 
-                    if(connection != null)
-                    {
-                        connection.Update(1);
+                    if (connection == null) continue;
 
-                        if(connection.connectionMode == Connection.ConnectionMode.DISCONNECTED)
-                        {
-                            lobbyConnections[i] = null;
-                        }
+                    connection.Update(tickDelay);
+
+                    if (connection.connectionMode == Connection.ConnectionMode.DISCONNECTED)
+                    {
+                        lobbyConnections[i] = null;
+
+                        Console.WriteLine("Removed connection " + connection.IP + ":" + connection.port + " from lobbyConnections");
                     }
                 }
 
-                Thread.Sleep((int)MathF.Floor(1f / 1f * 1000f));
+                Thread.Sleep((int)MathF.Floor(tickDelay * 1000f));
             }
         }
 
@@ -92,13 +154,15 @@ namespace BridgeServer
             if (packet.packetType == "HOST")
             {
                 string ID = Guid.NewGuid().ToString();
+                int maxConnections = packet.GetInt(0);
 
                 Console.WriteLine("Hosted room " + ID);
 
-                rooms.Add(new Room()
-                {
-                    ID = ID
-                });
+                Room room = new Room(ID, maxConnections);
+
+                room.connections[0] = connection;
+
+                rooms.Add(room);
 
                 connection.SendPacket(new Packet("HOST_INFO").AddValue(ID));
             }else if (packet.packetType == "JOIN")
@@ -113,15 +177,25 @@ namespace BridgeServer
 
                 if(room == null)
                 {
-                    connection.SendPacket(new Packet("JOIN_ERROR").AddValue("Room does not exist!"));
+                    connection.SendPacket(new Packet("JOIN_REJECTED").AddValue("Room does not exist!"));
 
                     return;
                 }
 
-                //Check if room is full
-                //connection.SendPacket(new Packet("JOIN_REJECTED").AddValue("Room is full!"));
+                int openIndex = room.GetOpenIndex();
 
-                //Add connection to room + remove from connections
+                if (openIndex == -1)
+                {
+                    connection.SendPacket(new Packet("JOIN_REJECTED").AddValue("Room is full!"));
+
+                    return;
+                }
+
+                room.connections[openIndex] = connection;
+
+                lobbyConnections[connectionIndex] = null;
+
+                Console.WriteLine("Offloaded connection " + connection.IP + ":" + connection.port + " from lobbyConnections.");
 
                 connection.SendPacket(new Packet("JOIN_ACCEPTED"));
             }
